8ef5fe227a39b44bf9226a8d7c108e06c31103eb
Nidhi <983643@win.edu.au>
2024-09-01 17:57:03 +1000

    Third Commit

---
+++ product.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Product Page</title>
+</head>
+<body>
+	<!-- <form action="calculate.php" method="POST">
+	</form> -->
+	<style type="text/css">
+		.container{
+			color: blue;
+		}
+
+
+	</style>
+
+	<div class='container'>
+		<div>
+			<img src="headphone.jpg" alt="Headphone" width="350" height="350">
+			Headphones
+		</div>
+
+	</div>
+
+	Headphones
+	$233.95
+
+	Blue Black Grey
+
+	Red
+
+
+</body>
+</html>
