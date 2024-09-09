73a291ffad6122ee2922e43e615e6c613d75fbe0
Nidhi <983643@win.edu.au>
2024-09-09 20:56:12 +1000

    Service page commit on 9 Sept

---
+++ services.php
@@ -0,0 +1,53 @@
+<?php
+	
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $product['name']; ?> - Product Page</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css"> <!-- Link to external CSS -->
+</head>
+<body>
+	<div class="service-page">
+		<div class="service-delivery">
+			<h1 class="serivce-heading">Delivery Options </h1>
+			When you buy online, we provide a range of convenient delivery options so you can get your purchases when you want. At checkout, we’ll show you the available options and delivery costs based on the items in your cart and where you need them delivered.<br>
+			<b>Standard Delivery (2 – 12 days)</b>:
+			This is the cheapest option depending on the size of the item and whether or not you want tracking. The estimated delivery date will vary as this depends on where you live and where the item is shipping from.<br>
+			<b>Express Delivery (1 – 2 business days):</b> 
+			Depending on the size and type of item you may be able to choose Express Delivery within the checkout. If you choose Express Delivery you’ll receive tracking notifications from our delivery partner once they have picked up your item(s) for delivery.<br>
+			<b>Courier Delivery (next business day):</b>
+			We offer next day delivery with Team Global Express Courier, available for deliveries to Melbourne, Sydney, Canberra, Brisbane, Perth, and Adelaide metro areas.<br>
+
+			‘Courier Delivery’ will show at checkout depending on the time you’re placing an order, items you’re ordering, their availability and if your items are coming from a store that accepts courier pickup. <br>
+
+			For more delivery options visit <a href="https://auspost.com.au/business/shipping/delivery-speeds-and-coverage">Australian Post Website </a>
+
+			<h1 class='serivce-heading'>Damages & Issues</h1>
+			Please inspect your order upon reception and contact us immediately if the item is defective, damaged or if you receive the wrong item, so that we can evaluate the issue and make it right. 
+
+			<h1 class="serivce-heading">Returns & Refunds</h1>
+			<b> Return Policy: </b> We have a 30-day return policy, which means you have 30 days after receiving your item to request a return.
+			To be eligible for a return, your item must be in the same condition that you received it, unworn or unused, with tags, and in its original packaging. You’ll also need the receipt or proof of purchase.<br>
+			If your return is accepted, we’ll send you a return shipping label, as well as instructions on how and where to send your package. Items sent back to us without first requesting a return will not be accepted.<br>
+			You can always contact us for any return question at support@hifi.com <br>
+
+			<b> Refund: </b> We will notify you once we’ve received and inspected your return, and let you know if the refund was approved or not. If approved, you’ll be automatically refunded on your original payment method within 10 business days. Please remember it can take some time for your bank or credit card company to process and post the refund too.
+
+
+
+
+		</div>
+	</div>
+</body>
+</html>
